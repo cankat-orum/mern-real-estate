9f0e5072c3ece06548d4489cf956002f0a43dcd7
cankat-orum <jankatorumanp@gmail.com>
2023-10-13 17:25:02 +0300

    ADD contact landlord functionality to the listing page (BROKEN)
    deleted userRef condition because it returned null

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 import {Swiper, SwiperSlide} from 'swiper/react'
 import SwiperCore from 'swiper'
 import {Navigation} from 'swiper/modules'
 import 'swiper/css/bundle'
+import Contact from '../components/Contact'
 
 
 export default function Listing() {
@@ -11,7 +13,9 @@ export default function Listing() {
     const [listing, setListing] = useState(null)
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
+    const [contact, setContact] = useState(false)
     const params = useParams()
+    const {currentUser} = useSelector((state) => state.user)
     useEffect(() => {
         const fetchListing = async () => {
             try {
@@ -42,6 +46,11 @@ export default function Listing() {
         {listing.imageUrls.map(url => <SwiperSlide key={url}><div className='h-[500px]' style={{background: `url(${url}) center no-repeat`, backgroundSize:'cover'}}></div></SwiperSlide>)
         }
     </Swiper>
-    </div>}</main>
+    </div>}
+    <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+        {currentUser && !contact && (<button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>Contact Landlord</button>)}
+        {contact && <Contact listing={listing}/>}
+    </div>
+    </main>
   )
 }
